caa9edef7efdaefd5a6c3b0a47acf5440eb91ac6
Cyytrus <antoniopaulo122@gmail.com>
2022-05-29 00:07:23 -0300

    Add libs

--- mix.exs
+++ mix.exs
@@ -44,7 +44,11 @@ defmodule GraphodaseQL.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:absinthe, "~> 1.6.0"},
+      {:absinthe_plug, "~> 1.5"},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"}
     ]
   end
 
